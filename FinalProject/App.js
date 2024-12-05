47d61b1d94870983d426fd244e1a26a2f3c2ab49
clairegaza <cgaza@villanova.edu>
2024-12-05 12:13:28 -0500

    moved files

--- FinalProject/App.js
+++ FinalProject/App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Home from './Home';
+import TopTracks from './TopTracks';
+import TopArtists from './TopArtists';
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    //Use the NavigationContainer and the Stack to display your app
+    //ListOfMovies is just here as a place holder.
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name='Home Page' component={Home} />
+        <Stack.Screen name='Top Tracks' component={TopTracks} /> 
+        <Stack.Screen name='Top Artists' component={TopArtists} /> 
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
